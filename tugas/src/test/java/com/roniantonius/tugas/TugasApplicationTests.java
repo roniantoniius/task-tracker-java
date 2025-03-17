fa025f43b01c9b6d998d0d2a79f337fb7efe9939
roniantonius <roniantoniuss@upi.edu>
2025-03-17 22:24:32 +0700

    Setting up task list project and the db

---
+++ tugas/src/test/java/com/roniantonius/tugas/TugasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roniantonius.tugas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TugasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
